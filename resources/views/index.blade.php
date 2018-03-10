20fb05884cde747fb64a4d01acc16ebe013e5e13
cymon1997 <cybermonster82@yahoo.com>
2018-03-10 12:45:12 +0700

    fix folder depedencies

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('main')
+
+@section('title', config('app.name'))
+
+@section('extra-css')
+<!-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> -->
+@endsection
+
+@section('content')
+<div class="container">
+    
+</div>
+@endsection
+
+@section('extra-js')
+<!-- <script type="application/javascript" src="{{ asset('js/app.js') }}"></script> -->
+@endsection
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ config('app.locale') }}" dir="rtl">
+<html lang="{{ config('app.locale') }}" dir="ltr">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -32,30 +32,39 @@
         <!-- Google Open Sans -->
         <!-- <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> -->
 
-        <title>Title | @yield('title')</title>
+        <title>@yield('title')</title>
     </head>
     <body>
-        @include('layout.header')
+        @include('layouts.header')
         <div class="">
         @yield('content')
         </div>
 
         <!-- JQuery -->
         <script type="application/javascript" src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
-        <!-- <script type="application/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script> -->
+        <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script> -->
+        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script> -->
+
+        <!-- Bootstrap -->
+        <script type="application/javascript" src="{{ asset('js/bootstrap-v4.0.min.js') }}"></script>
+        <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
+        <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> -->
+
         <!-- PopperJS -->
         <!-- <script type="application/javascript" src="https://unpkg.com/popper.js"></script> -->
+
         <!-- TooltipJS -->
         <!-- <script type="application/javascript" src="https://unpkg.com/tooltip.js"></script> -->
-        <!-- Bootstrap -->
-        <!-- <script type="application/javascript" src="{{ asset('js/bootstrap-v4.0.min.js') }}"></script> -->
+
         <!-- Foundation ZURB -->
-        <script type="application/javascript" src="{{ asset('js/what-input.js') }}"></script>
-        <script type="application/javascript" src="{{ asset('js/foundation.min.js') }}"></script>
+        <!-- <script type="application/javascript" src="{{ asset('js/what-input.js') }}"></script> -->
+        <!-- <script type="application/javascript" src="{{ asset('js/foundation.min.js') }}"></script> -->
+
         <!-- Customization -->
         <script type="application/javascript" src="{{ asset('js/header.js') }}"></script>
+        <script type="application/javascript" src="{{ asset('js/footer.js') }}"></script>
         @yield('extra-js')
 
-        @include('layout.footer')
+        @include('layouts.footer')
     </body>
 </html>
